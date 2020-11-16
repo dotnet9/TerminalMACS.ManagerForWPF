9f0ba99dd7927fcf80116e0aa39710caba978ccb
dotnet9 <632871194@qq.com>
2020-11-17 06:39:57 +0800

    添加DotNetty客户端

---
+++ src/Tools/NettyTest/DotNettyClient/Models/ChatInfoModel.cs
@@ -0,0 +1,22 @@
+﻿using HandyControl.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNettyClient.Models
+{
+    public struct ChatInfoModel
+    {
+        public object Message { get; set; }
+
+        public string SenderId { get; set; }
+
+        public ChatRoleType Role { get; set; }
+
+        public ChatMessageType Type { get; set; }
+
+        public object Enclosure { get; set; }
+    }
+}
---
+++ src/Tools/NettyTest/DotNettyClient/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using DotNettyClient.Models;
+using HandyControl.Data;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DotNettyClient.ViewModel
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ObservableCollection<ChatInfoModel> ChatInfos { get; set; } = new ObservableCollection<ChatInfoModel>();
+        private string _ChatString;
+
+        public string ChatString
+        {
+            get { return _ChatString; }
+            set { SetProperty(ref _ChatString, value); }
+        }
+        public ICommand RaiseSendStringCommand;
+
+
+        private readonly string _id = Guid.NewGuid().ToString();
+
+        public MainWindowViewModel()
+        {
+            RaiseSendStringCommand = new DelegateCommand(RaiseSendStringHandler);
+        }
+
+        private void RaiseSendStringHandler()
+        {
+            if (string.IsNullOrEmpty(ChatString)) return;
+            var info = new ChatInfoModel
+            {
+                Message = ChatString,
+                SenderId = _id,
+                Type = ChatMessageType.String,
+                Role = ChatRoleType.Sender
+            };
+            ChatInfos.Add(info);
+            ChatString = string.Empty;
+        }
+
+        private void ReceiveMessage(ChatInfoModel info)
+        {
+            info.Role = ChatRoleType.Receiver;
+            ChatInfos.Add(info);
+        }
+    }
+}
