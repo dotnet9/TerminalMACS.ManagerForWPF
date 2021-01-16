94506781b0754b425a34887538506862480aa1c3
dotnet9 <632871194@qq.com>
2021-01-17 00:12:02 +0800

    添加Quartz.Net测试

---
+++ src/Demo/QuartzNETTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzNETTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Task.Run(async () => await TestJob());
+			Console.ReadKey();
+		}
+
+		static async Task TestJob()
+		{
+
+			// 创建任务
+			IJobDetail jobDetail = JobBuilder.Create<TestJob>()
+				.WithIdentity("TestJob", "Test")
+				.Build();
+
+			// 触发条件
+			ITrigger trigger = TriggerBuilder.Create()
+				.WithIdentity("TestJobTrigger", "Test")
+				.WithSimpleSchedule(x =>
+				{
+					x.WithIntervalInSeconds(3).RepeatForever();
+				})
+				.Build();
+
+			// 启动任务
+			StdSchedulerFactory stdSchedulerFactory = new StdSchedulerFactory();
+			IScheduler scheduler = await stdSchedulerFactory.GetScheduler();
+			await scheduler.Start();
+			await scheduler.ScheduleJob(jobDetail, trigger);
+		}
+	}
+}
---
+++ src/Demo/QuartzNETTest/TestJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzNETTest
+{
+	public class TestJob : IJob
+	{
+		public async Task Execute(IJobExecutionContext context)
+		{
+			await Console.Out.WriteLineAsync(DateTime.Now.ToString());
+		}
+	}
+}
