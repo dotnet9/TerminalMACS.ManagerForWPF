91f18855e0546d1293ecfcb36366f9ccbdd56f93
dotnet9 <632871194@qq.com>
2021-04-09 22:23:53 +0800

    添加测试代码

--- src/Demo/ConsoleAppForDotnet6/Program.cs
+++ src/Demo/ConsoleAppForDotnet6/Program.cs
@@ -7,34 +7,99 @@ namespace ConsoleAppForDotnet6
 	{
 		static void Main(string[] args)
 		{
-			Employee mike = new Employee()
+			// 使用接口工MyEnumerable代替MyList
+			IMyEnumerable list = new MyList();
+			// 得到迭代器，在循环中针对迭代嚣编码，而不是集合MyList
+			IMyEnumerator enumerator = list.GetEnumerator();
+			for (int i = 0; i < list.Count; i++)
 			{
-				IDCode = "NB123",
-				Age = 30,
-				Department = new Department
-				{
-					Name = "Dep1"
-				}
-			};
+				object current = enumerator.Current;
+				enumerator.MoveNext();
+			}
 
-			Employee rose = mike.Clone() as Employee;
+			while (enumerator.MoveNext())
+			{
+				object current = enumerator.Current;
+			}
+		}
+	}
 
-			Console.WriteLine(rose.IDCode);
-			Console.WriteLine(rose.Age);
-			Console.WriteLine(rose.Department);
+	/// <summary>
+	/// 要求所有的迭代器全部实现该接口
+	/// </summary>
+	interface IMyEnumerator
+	{
+		bool MoveNext();
+		object Current { get; }
+	}
 
-			Console.WriteLine("开始改变mike的值: ");
+	/// <summary>
+	/// 要求所有的集合实现该接口
+	/// 这样一来，客户端就可以针对该接口编码
+	/// 而无需关注具体的实现
+	/// </summary>
+	interface IMyEnumerable
+	{
+		IMyEnumerator GetEnumerator();
+		int Count { get; }
+	}
 
-			mike.IDCode = "NB456";
-			mike.Age = 60;
-			mike.Department.Name = "Dep2";
+	class MyList : IMyEnumerable
+	{
+		object[] items = new object[10];
+		IMyEnumerator myEnumerator;
 
-			Console.WriteLine(rose.IDCode);
-			Console.WriteLine(rose.Age);
-			Console.WriteLine(rose.Department);
+		public object this[int i]
+		{
+			get { return items[i]; }
+			set { this.items[i] = value; }
+		}
+
+		public int Count
+		{
+			get { return items.Length; }
+		}
+
+		public IMyEnumerator GetEnumerator()
+		{
+			if (myEnumerator == null)
+			{
+				myEnumerator = new MyEnumerator(this);
+			}
+			return myEnumerator;
 
 		}
 	}
-}
 
+	class MyEnumerator : IMyEnumerator
+	{
+		int index = 0;
+		MyList myList;
+
+		public MyEnumerator(MyList myList)
+		{
+			this.myList = myList;
+		}
+
+		public bool MoveNext()
+		{
+			if (index + 1 > myList.Count)
+			{
+				index = 1;
+				return false;
+			}
+			else
+			{
+				index++;
+				return true;
+			}
+		}
+
+		public object Current
+		{
+			get { return myList[index - 1]; }
+		}
+	}
+
+}
 
