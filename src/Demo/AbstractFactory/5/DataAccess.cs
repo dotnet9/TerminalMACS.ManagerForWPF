6d3e96b1b408d2fafc8a79d9e76a64509a9b6199
沙漠尽头的狼(dotnet9.com) <1012434131@qq.com>
2021-04-11 15:27:50 +0800

    Update DataAccess.cs

--- src/Demo/AbstractFactory/5/DataAccess.cs
+++ src/Demo/AbstractFactory/5/DataAccess.cs
@@ -1,12 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AbstractFactory._4;
+using System.Configuration;
+
+//引入反射，必须要写
+using System.Reflection;
 
 namespace AbstractFactory._5
 {
   class DataAccess
   {
+    // 程序集名称："抽象工厂模式"
+    private static readonly string AssemblyName = "抽象工厂模式";
+
+    // 数据库名称，可以替换成Access
+    private static readonly string db = "sqlserver";
+
+    public static IUser CreateUser()
+    {
+      string className = AssemblyName + "." + db + "User";
+      return (IUser)Assembly.Load(AssemblyName).CreateInstance(className);
+    }
+
+    public static IDepartment CreateDepartment()
+    {
+      string className = AssemblyName + "." + db + "Department";
+      return (IDepartment)Assembly.Load(AssemblyName).CreateInstance(className);
+    }
   }
 }
