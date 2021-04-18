dd218538d91f1800aa54639172b720602126d4bd
dotnet9 <632871194@qq.com>
2021-04-18 12:53:54 +0800

    添加WPF日志可视化面板支持

---
+++ src/Demo/WPFWithLogDashboard/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Serilog;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WPFWithLogDashboard
+{
+	public partial class MainWindow : Window
+	{
+		private ILogger logger;
+		public ILogger MyLoger
+		{
+			get
+			{
+				if (logger == null)
+				{
+					logger = Log.ForContext<MainWindow>();
+				}
+				return logger;
+			}
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			MyLoger.Information("WPF窗体中记录的日志");
+		}
+		private void AddInfoLog_Click(object sender, RoutedEventArgs e)
+		{
+			MyLoger.Information("测试添加Info日志");
+
+		}
+
+		private void AddErrorLog_Click(object sender, RoutedEventArgs e)
+		{
+			MyLoger.Error("测试添加异常日志");
+		}
+
+		private void OpenLogDashboard_Click(object sender, RoutedEventArgs e)
+		{
+			OpenUrl("http://localhost:5000/logdashboard");
+		}
+
+		private void OpenUrl(string url)
+		{
+			Process.Start(new ProcessStartInfo("cmd", $"/c start {url}")
+			{
+				UseShellExecute = false,
+				CreateNoWindow = true
+			});
+		}
+	}
+}
---
+++ src/Demo/WPFWithLogDashboard/Startup.cs
@@ -0,0 +1,46 @@
+﻿using LogDashboard;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace WPFWithLogDashboard
+{
+	public class Startup
+	{
+		private ILogger logger;
+		public ILogger MyLoger
+		{
+			get
+			{
+				if (logger == null)
+				{
+					logger = Log.ForContext<Startup>();
+				}
+				return logger;
+			}
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			MyLoger.Information("ConfigureServices");
+
+			services.AddLogDashboard();
+			services.AddControllers();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			MyLoger.Information("Configure");
+
+			app.UseLogDashboard();
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
