d5f7919ad3a88c4c74c42652f9819af6cac60656
dotnet9 <632871194@qq.com>
2021-05-24 23:14:30 +0800

    完善部署排序算法

--- src/TerminalMACS.Server/Models/QuickSortingAlgorithm.cs
+++ src/TerminalMACS.Server/Models/QuickSortingAlgorithm.cs
@@ -8,72 +8,71 @@ namespace TerminalMACS.Server.Models
 	{
 		public void Sort(List<KeyValuePair<string, double>> data, Action<List<KeyValuePair<string, double>>> updateData)
 		{
-			Sort(data, updateData, 0, data.Count - 1);
+			QuickSort(data, updateData, 0, data.Count - 1);
 		}
 
-		private void Sort(List<KeyValuePair<string, double>> data, Action<List<KeyValuePair<string, double>>> updateData, int low, int high)
+		private void QuickSort(
+			List<KeyValuePair<string, double>> data,
+			Action<List<KeyValuePair<string, double>>> updateData,
+			int begin,
+			int end)
 		{
-
-			KeyValuePair<string, double> pivot;//存储分支点    
-			int l, r;
-			int mid;
-			if (high <= low)
+			// 两个指针重合就返回，结束调用
+			if (begin >= end)
 			{
 				return;
 			}
 
-			if (high == (low + 1))
-			{
-				if (data[low].Value > data[high].Value)
-				{
-					swap(data, updateData, low, high);
-				}
-				return;
-			}
+			//会得到一个基准值下标
+			int pivotIndex = QuickSort_Once(data, updateData, begin, end);
 
-			mid = (low + high) >> 1;
-			pivot = data[mid];
-			swap(data, updateData, low, mid);
-			l = low + 1;
-			r = high;
+			//对基准的左端进行排序  递归
+			QuickSort(data, updateData, begin, pivotIndex - 1);
 
-			do
-			{
-				while (l <= r && data[l].Value < pivot.Value)
-				{
-					l++;
-				}
-
-				while (data[r].Value >= pivot.Value)
-				{
-					r--;
-					if (l < r)
-					{
-						swap(data, updateData, l, r);
-					}
-				}
-			} while (l < r);
-
-			data[low] = data[r];
-			data[r] = pivot;
-			if ((low + 1) < r)
-			{
-				Sort(data, updateData, low, (r - 1));
-			}
-			if ((r + 1) < high)
-			{
-				Sort(data, updateData, (r + 1), high);
-			}
+			//对基准的右端进行排序  递归
+			QuickSort(data, updateData, pivotIndex + 1, end);
 		}
 
 
-		private void swap(List<KeyValuePair<string, double>> data, Action<List<KeyValuePair<string, double>>> updateData, int change1Index, int change2Index)
+		private int QuickSort_Once(
+			List<KeyValuePair<string, double>> data,
+			Action<List<KeyValuePair<string, double>>> updateData,
+			int begin,
+			int end)
 		{
-			var temp = data[change1Index];
-			data[change1Index] = data[change2Index];
-			data[change2Index] = temp;
+			//将首元素作为基准
+			var pivot = data[begin];
+			int i = begin;
+			int j = end;
 
+			while (i < j)
+			{
+				//从右到左，寻找第一个小于基准pivot的元素
+				while (data[j].Value >= pivot.Value && i < j)
+				{
+					j--; //指针向前移
+				}
+
+				//执行到此，j已指向从右端起第一个小于基准pivot的元素，执行替换
+				data[i] = data[j];
+				updateData(data);
+
+				//从左到右，寻找首个大于基准pivot的元素
+				while (data[i].Value <= pivot.Value && i < j)
+				{
+					i++; //指针向后移
+				}
+
+				//执行到此,i已指向从左端起首个大于基准pivot的元素，执行替换
+				data[j] = data[i];
+				updateData(data);
+			}
+
+			//退出while循环,执行至此，必定是 i= j的情况（最后两个指针会碰头）
+			//i(或j)所指向的既是基准位置，定位该趟的基准并将该基准位置返回
+			data[i] = pivot;
 			updateData(data);
+			return i;
 		}
 	}
 }
--- src/TerminalMACS.Server/Views/ChatTest.xaml.cs
+++ src/TerminalMACS.Server/Views/ChatTest.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using TerminalMACS.Controls.Controls;
 using TerminalMACS.Server.Models;
 
 namespace TerminalMACS.Server.Views
@@ -14,31 +17,57 @@ namespace TerminalMACS.Server.Views
 	{
 		List<KeyValuePair<string, double>> data = new List<KeyValuePair<string, double>>();
 		Dictionary<string, ISortingAlgorithm> dictSortingAlgorithms = new Dictionary<string, ISortingAlgorithm>();
+		List<TextBlock> lstSortNameTextBlocks = new List<TextBlock>();
+		List<WpfChart> lstSortCharts = new List<WpfChart>();
 		Random rd = new Random(DateTime.Now.Millisecond);
+		private int sleepMs = 50;
+
+
 		public ChatTest()
 		{
 			InitializeComponent();
 
-			//dictSortingAlgorithms["选择排序算法"] = new SelectionSortingAlgorithm();
-			//dictSortingAlgorithms["冒泡排序算法"] = new EbullitionSortingAlgorithm();
-			//dictSortingAlgorithms["快速排序算法"] = new QuickSortingAlgorithm();
-			//dictSortingAlgorithms["插入排序算法"] = new InsertionSortingAlgorithm();
+			dictSortingAlgorithms["选择排序算法"] = new SelectionSortingAlgorithm();
+			dictSortingAlgorithms["冒泡排序算法"] = new EbullitionSortingAlgorithm();
+			dictSortingAlgorithms["快速排序算法"] = new QuickSortingAlgorithm();
+			dictSortingAlgorithms["插入排序算法"] = new InsertionSortingAlgorithm();
 			dictSortingAlgorithms["希尔排序"] = new ShellSortingAlgorithm();
+
+			lstSortNameTextBlocks.Add(tbSortName1);
+			lstSortNameTextBlocks.Add(tbSortName2);
+			lstSortNameTextBlocks.Add(tbSortName3);
+			lstSortNameTextBlocks.Add(tbSortName4);
+			lstSortNameTextBlocks.Add(tbSortName5);
+
+			lstSortCharts.Add(chart1);
+			lstSortCharts.Add(chart2);
+			lstSortCharts.Add(chart3);
+			lstSortCharts.Add(chart4);
+			lstSortCharts.Add(chart5);
+
+			for (int i = 0; i < lstSortCharts.Count; i++)
+			{
+				ChangeSoringAlgorithmName(lstSortNameTextBlocks[i], dictSortingAlgorithms.Keys.ToList()[i]);
+			}
 		}
 
 		private void Begin_Click(object sender, RoutedEventArgs e)
 		{
 			this.EnableUIBtn(false);
-			ThreadPool.QueueUserWorkItem(sen =>
+			lstSortCharts.ForEach(chart =>
 			{
-				foreach (var kvp in dictSortingAlgorithms)
+				int index = lstSortCharts.IndexOf(chart);
+				var sortAlgorithms = dictSortingAlgorithms.Values.ToList()[index];
+				ThreadPool.QueueUserWorkItem(sen =>
 				{
-					this.ChangeSoringAlgorithmName(kvp.Key);
 					var tempData = CopyArray(data);
-					kvp.Value.Sort(tempData, this.UpdateChart);
+					sortAlgorithms.Sort(tempData, newDatas =>
+					{
+						UpdateChart(chart, newDatas);
+					});
 
 					Thread.Sleep(TimeSpan.FromSeconds(2));
-				}
+				});
 				this.EnableUIBtn(true);
 			});
 		}
@@ -57,25 +86,36 @@ namespace TerminalMACS.Server.Views
 			{
 				numberCount = 20;
 			}
+
 			for (int i = 0; i < numberCount; i++)
 			{
 				data.Add(new KeyValuePair<string, double>((i + 1).ToString(), rd.Next(5, 100)));
 			}
-			UpdateChart(data);
+
+			if(!int.TryParse(this.tbSleepMs.Text, out var sleepMsTmp))
+			{
+				sleepMs = 150;	
+			}
+			else
+			{
+				sleepMs = sleepMsTmp;
+			}
+
+			lstSortCharts.ForEach(cu => UpdateChart(cu, data));
 		}
 
 		/// <summary>
 		/// 更新柱状图
 		/// </summary>
 		/// <param name="arrs"></param>
-		private void UpdateChart(List<KeyValuePair<string, double>> arrs)
+		private void UpdateChart(WpfChart chart, List<KeyValuePair<string, double>> arrs)
 		{
 			this.Dispatcher.Invoke(() =>
 			{
-				this.chart.ItemsSource = null;
-				this.chart.ItemsSource = arrs;
+				chart.ItemsSource = null;
+				chart.ItemsSource = arrs;
 			});
-			Thread.Sleep(TimeSpan.FromMilliseconds(500));
+			Thread.Sleep(TimeSpan.FromMilliseconds(sleepMs));
 		}
 
 		private List<KeyValuePair<string, double>> CopyArray(List<KeyValuePair<string, double>> arrs)
@@ -96,11 +136,11 @@ namespace TerminalMACS.Server.Views
 			});
 		}
 
-		private void ChangeSoringAlgorithmName(string name)
+		private void ChangeSoringAlgorithmName(TextBlock tb, string name)
 		{
 			this.Dispatcher.Invoke(() =>
 			{
-				this.tbSortingAlgorithmName.Text = name;
+				tb.Text = name;
 			});
 		}
 	}
