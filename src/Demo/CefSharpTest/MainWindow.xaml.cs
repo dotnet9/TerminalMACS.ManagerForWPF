f0dc0f4fd9b578b1ea6e8336255d1e73fb006450
Dotnet9(dotnet9.com) <1012434131@qq.com>
2021-06-09 10:15:17 +0800

    add cefsharp test project

---
+++ src/Demo/CefSharpTest/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+
+namespace CefSharpTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Loaded += (s, e) =>
+			{
+				browser.Address = $"{AppDomain.CurrentDomain.BaseDirectory}test.html";
+			};
+		}
+	}
+}
