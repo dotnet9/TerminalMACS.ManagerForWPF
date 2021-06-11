b973bbff7a44a809d9a96fad3b7ea0b968432de6
Dotnet9(dotnet9.com) <1012434131@qq.com>
2021-06-11 09:55:43 +0800

    Add vc++ library to CefSharp project

--- src/Demo/CefSharpTest/App.xaml.cs
+++ src/Demo/CefSharpTest/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CefSharp;
 using CefSharp.Wpf;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -8,6 +9,8 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -18,6 +21,38 @@ namespace CefSharpTest
 	/// </summary>
 	public partial class App : Application
 	{
+		private static readonly string[] DllList =
+		{
+			"api-ms-win-core-console-l1-1-0.dll", "api-ms-win-core-datetime-l1-1-0.dll",
+			"api-ms-win-core-debug-l1-1-0.dll", "api-ms-win-core-errorhandling-l1-1-0.dll",
+			"api-ms-win-core-file-l1-1-0.dll", "api-ms-win-core-file-l1-2-0.dll",
+			"api-ms-win-core-file-l2-1-0.dll", "api-ms-win-core-handle-l1-1-0.dll",
+			"api-ms-win-core-heap-l1-1-0.dll", "api-ms-win-core-interlocked-l1-1-0.dll",
+			"api-ms-win-core-libraryloader-l1-1-0.dll", "api-ms-win-core-localization-l1-2-0.dll",
+			"api-ms-win-core-memory-l1-1-0.dll", "api-ms-win-core-namedpipe-l1-1-0.dll",
+			"api-ms-win-core-processenvironment-l1-1-0.dll", "api-ms-win-core-processthreads-l1-1-0.dll",
+			"api-ms-win-core-processthreads-l1-1-1.dll", "api-ms-win-core-profile-l1-1-0.dll",
+			"api-ms-win-core-rtlsupport-l1-1-0.dll", "api-ms-win-core-string-l1-1-0.dll",
+			"api-ms-win-core-synch-l1-1-0.dll", "api-ms-win-core-synch-l1-2-0.dll",
+			"api-ms-win-core-sysinfo-l1-1-0.dll", "api-ms-win-core-timezone-l1-1-0.dll",
+			"api-ms-win-core-util-l1-1-0.dll", "api-ms-win-crt-conio-l1-1-0.dll",
+			"api-ms-win-crt-convert-l1-1-0.dll", "api-ms-win-crt-environment-l1-1-0.dll",
+			"api-ms-win-crt-filesystem-l1-1-0.dll", "api-ms-win-crt-heap-l1-1-0.dll",
+			"api-ms-win-crt-locale-l1-1-0.dll", "api-ms-win-crt-math-l1-1-0.dll",
+			"api-ms-win-crt-multibyte-l1-1-0.dll", "api-ms-win-crt-private-l1-1-0.dll",
+			"api-ms-win-crt-process-l1-1-0.dll", "api-ms-win-crt-runtime-l1-1-0.dll",
+			"api-ms-win-crt-stdio-l1-1-0.dll", "api-ms-win-crt-string-l1-1-0.dll",
+			"api-ms-win-crt-time-l1-1-0.dll", "api-ms-win-crt-utility-l1-1-0.dll",
+			"ucrtbase.dll"
+		};
+
+		private static bool _dllLoaded;
+
+		static App()
+		{
+			_dllLoaded = IsVc2015Installed();
+		}
+
 		public App()
 		{
 			//Add Custom assembly resolver
@@ -48,6 +83,7 @@ namespace CefSharpTest
 		{
 			if (args.Name.StartsWith("CefSharp"))
 			{
+				CheckDll();
 				string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
 				string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
 					Environment.Is64BitProcess ? "x64" : "x86",
@@ -59,6 +95,70 @@ namespace CefSharpTest
 			}
 
 			return null;
+		}
+
+
+
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr LoadLibrary(string lpFileName);
+
+		public static bool IsVc2015Installed()
+		{
+			var dependenciesPath = @"SOFTWARE\Classes\Installer\Dependencies";
+			var plat = Environment.Is64BitProcess ? "x64" : "x86";
+			using (var dependencies = Registry.LocalMachine.OpenSubKey(dependenciesPath))
+			{
+				if (dependencies == null)
+				{
+					return false;
+				}
+
+				foreach (var subKeyName in dependencies.GetSubKeyNames().Where(n =>
+					!n.ToLower().Contains("dotnet") && !n.ToLower().Contains("microsoft")))
+				{
+					using (var subDir = Registry.LocalMachine.OpenSubKey(dependenciesPath + "\\" + subKeyName))
+					{
+						var value = subDir.GetValue("DisplayName")?.ToString() ?? null;
+						if (string.IsNullOrEmpty(value))
+						{
+							continue;
+						}
+
+						if (Regex.IsMatch(value, $@"C\+\+ 2015.*\({plat}\)")) //here u can specify your version.
+						{
+							return true;
+						}
+					}
+				}
+			}
+
+			return false;
+		}
+
+		private static void CheckDll() // 检查浏览器的DLL是否载入
+		{
+			if (_dllLoaded)
+			{
+				return;
+			}
+
+			var dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Environment.Is64BitProcess ? "x64" : "x86");
+			foreach (var fname in DllList)
+			{
+				try
+				{
+					var path = Path.Combine(dir, fname);
+					if (File.Exists(path))
+					{
+						LoadLibrary(path);
+					}
+				}
+				catch
+				{
+				}
+			}
+
+			_dllLoaded = true;
 		}
 	}
 }
