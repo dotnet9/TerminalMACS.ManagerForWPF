6a65a7364fb1102735b2c4c6de4cefedd3961f5d
liushiquan <liushiquan@islide.cc>
2021-10-26 15:22:31 +0800

    add web image loading demo;

---
+++ src/Demo/LazyLoadWebImage/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace LazyLoadWebImage
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private bool isLoaded = false;
+		private void SetBinding_Click(object sender, RoutedEventArgs e)
+		{
+			if(isLoaded)
+			{
+				return;
+			}
+			isLoaded = true;
+
+			MainWindowViewModel viewModel = new MainWindowViewModel();
+			this.lvImages.ItemsSource = viewModel.WebImages;
+		}
+	}
+}
