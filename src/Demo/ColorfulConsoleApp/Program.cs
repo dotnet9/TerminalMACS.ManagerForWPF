69e3fafe31f7335a160b70ba36cdb8841272fdf0
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-04-04 12:58:30 +0800

    add colorful console project;

---
+++ src/Demo/ColorfulConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+using Colorful;
+using Console = Colorful.Console;
+
+FigletFont font = FigletFont.Load("chunky.flf");
+Figlet figlet = new Figlet(font);
+
+Console.WriteLine(figlet.ToAscii("Belvedere"), ColorTranslator.FromHtml("#8AFFEF"));
+Console.WriteLine(figlet.ToAscii("ice"), ColorTranslator.FromHtml("#FAD6FF"));
+Console.WriteLine(figlet.ToAscii("cream."), ColorTranslator.FromHtml("#B8DBFF"));
