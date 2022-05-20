0569faf6da66d3db19d4b9d7ac0090ae154f8a76
Dotnet9 <1012434131@qq.com>
2022-05-20 15:17:47 +0800

    remove empty project;

---
+++ src/Demo/WpfAppDemo/HintUC.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleGuide;
+
+/// <summary>
+/// Interaction logic for HintUC.xaml
+/// </summary>
+public partial class HintUC : UserControl
+{
+    public HintUC(string xh, string content, Visibility vis = Visibility.Visible, int width = 260, int height = 160)
+    {
+        InitializeComponent();
+        this.Width = width;
+        this.Height = height;
+        this.tb_nr.Width = width / 4;
+
+        this.tb_xh.Text = xh;
+        this.tb_nr.Text = content;
+        this.btn_next.Visibility = vis;
+    }
+
+    public delegate void NextHintDelegate();
+
+    public event NextHintDelegate nextHintEvent;
+
+    private void btn_next_Click(object sender, RoutedEventArgs e)
+    {
+        nextHintEvent();
+    }
+
+    private void btn_close_Click(object sender, RoutedEventArgs e)
+    {
+        Window.GetWindow(this).Close();
+    }
+}
--- src/Demo/WpfAppDemo/MainWindow.xaml.cs
+++ src/Demo/WpfAppDemo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
-namespace WpfAppDemo;
+namespace SimpleGuide;
 
 /// <summary>
 ///     Interaction logic for MainWindow.xaml
@@ -10,5 +11,31 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        List<GuidVo> list = new List<GuidVo>()
+        {
+            new GuidVo()
+            {
+                Uc=btn1,
+                Content="我是button，是第一步"
+            },
+            new GuidVo()
+            {
+                Uc=tb1,
+                Content="我是textbox，是第二步"
+            },
+            new GuidVo()
+            {
+                Uc=rb1,
+                Content="我是RadioButton，是第三步，也是最后一步了"
+            }
+        };
+
+        GuideWin win = new GuideWin(this, list);
+
+        win.ShowDialog();
     }
 }
