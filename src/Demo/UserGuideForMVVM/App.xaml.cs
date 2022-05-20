cbb6ce1b8c827e7bda1425c23cb009ecffe98939
Dotnet9 <1012434131@qq.com>
2022-05-20 18:10:45 +0800

    添加用户引导工程（MVVM方式），空工程；

---
+++ src/Demo/UserGuideForMVVM/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace UserGuideForMVVM;
+
+/// <summary>
+/// 参考开源项目：https://gitee.com/akwkevin/aistudio.-wpf.-acontrol
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ src/Demo/UserGuideForMVVM/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace UserGuideForMVVM;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
