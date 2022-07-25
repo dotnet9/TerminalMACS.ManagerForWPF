ccfd91e36570d6c650af45f3ca8aaf0643f80e43
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-07-25 21:29:25 +0800

    添加文字转语音测试；

---
+++ src/Demo/ConvertWordToSound/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ConvertWordToSound;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ src/Demo/ConvertWordToSound/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ConvertWordToSound;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
