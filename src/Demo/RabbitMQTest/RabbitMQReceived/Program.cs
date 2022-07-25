e234ce8244eb214853b8466ea6804ed9160fd800
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-07-25 22:59:13 +0800

    添加RabbitMQ测试程序；

---
+++ src/Demo/RabbitMQTest/RabbitMQReceived/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.DispatchConsumersAsync = true;
+const string exchangeName = "exchange1";
+const string eventName = "myEvent";
+using var conn = factory.CreateConnection();
+using var channel = conn.CreateModel();
+var queueName = "queue1";
+channel.ExchangeDeclare(exchangeName, "direct");
+channel.QueueDeclare(queueName, true, false, false, null);
+channel.QueueBind(queueName, exchangeName, eventName);
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.Received += Consumer_Received;
+channel.BasicConsume(queueName, false, consumer);
+Console.ReadLine();
+
+async Task Consumer_Received(object sender, BasicDeliverEventArgs args)
+{
+    try
+    {
+        var bytes = args.Body.ToArray();
+        var msg = Encoding.UTF8.GetString(bytes);
+        Console.WriteLine($"{DateTime.Now}收到了消息{msg}");
+        channel.BasicAck(args.DeliveryTag, false);
+        await Task.Delay(TimeSpan.FromMilliseconds(800));
+    }
+    catch (Exception ex)
+    {
+        channel.BasicReject(args.DeliveryTag, true);
+        Console.WriteLine($"处理收到的消息出错{ex}");
+    }
+}
---
+++ src/Demo/RabbitMQTest/RabbitMQSend/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.DispatchConsumersAsync = true;
+const string exchangeName = "exchange1";
+const string eventName = "myEvent";
+using var conn = factory.CreateConnection();
+while (true)
+{
+    var msg = DateTime.Now.TimeOfDay.ToString();
+    using (var channel = conn.CreateModel())
+    {
+        var properties = channel.CreateBasicProperties();
+        properties.DeliveryMode = 2;
+        channel.ExchangeDeclare(exchangeName, "direct");
+        var body = Encoding.UTF8.GetBytes(msg);
+        channel.BasicPublish(exchangeName, eventName, true, properties, body);
+    }
+
+    Console.WriteLine($"发布了消息：{msg}");
+    Thread.Sleep(TimeSpan.FromSeconds(1));
+}
