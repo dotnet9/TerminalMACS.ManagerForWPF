ebbccd1fc3128c69809c8c0089feaf2c23b20f09
Dotnet9 <1012434131@qq.com>
2022-08-19 11:21:35 +0800

    简单把拖拽处理放ViewModel;

--- src/Demo/WpfAppForZoomInAndZoomOut/ViewModels/ChildView1ViewModel.cs
+++ src/Demo/WpfAppForZoomInAndZoomOut/ViewModels/ChildView1ViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using Prism.Mvvm;
 using WpfAppForZoomInAndZoomOut.Models;
 
@@ -8,6 +12,7 @@ namespace WpfAppForZoomInAndZoomOut.ViewModels;
 
 public class ChildView1ViewModel : BindableBase //, IDropTarget
 {
+    private bool isDraging;
     public const int MoveStep = 5;
     private int _dragInsertPosition;
     private TreeItemModel? _dragSourceItem;
@@ -62,5 +67,97 @@ public class ChildView1ViewModel : BindableBase //, IDropTarget
     public void UpdatePage()
     {
         for (var i = 0; i < ItemSource.Count; i++) ItemSource[i].Index = i + 1;
+    }
+
+    public void DragStart(object sender, MouseButtonEventArgs e)
+    {
+        var parent = (ListBox)sender;
+        SourcePosition = e.GetPosition(parent);
+        var hitTestControl = VisualTreeHelper.HitTest(parent, SourcePosition);
+
+        if (hitTestControl is
+            { VisualHit: Path { Name: "Path_DragPosition" } or Border { Name: "Border_DragPosition" } })
+        {
+            DragSourceItem =
+                GetDataFromListBox(parent, SourcePosition) as TreeItemModel;
+            if (DragSourceItem != null)
+            {
+                isDraging = true;
+                DragDrop.DoDragDrop(parent, DragSourceItem, DragDropEffects.Move);
+            }
+            else
+            {
+                ClearDragInfo();
+            }
+        }
+        else
+        {
+            ClearDragInfo();
+        }
+    }
+
+    public void DragOver(object sender, DragEventArgs e)
+    {
+        if (!isDraging) return;
+
+        var parent = (ListBox)sender;
+        TargetPosition = e.GetPosition(parent);
+        var targetItem = GetDataFromListBox(parent, TargetPosition) as TreeItemModel;
+        if (DragSourceItem != targetItem)
+        {
+            this.DragTargetItem = targetItem;
+            e.Effects = DragDropEffects.Move;
+            DragInsertPosition = (int)((TargetPosition.X - SourcePosition.X) / 5);
+        }
+        else
+        {
+            this.DragTargetItem = null;
+            e.Effects = DragDropEffects.None;
+        }
+    }
+
+    public void Drop(object sender, DragEventArgs e)
+    {
+        var parent = (ListBox)sender;
+
+        if (!isDraging || DragSourceItem == null || DragTargetItem == null) return;
+
+
+        var sourceItemIndex = ItemSource.IndexOf(DragSourceItem);
+        var targetItemIndex = ItemSource.IndexOf(DragTargetItem);
+
+        var left = DragTargetItem.Margin.Left;
+        left += ChildView1ViewModel.MoveStep * DragInsertPosition;
+
+        DragSourceItem.Margin = new Thickness(left, 0, 0, 0);
+        ItemSource.Move(sourceItemIndex,
+            sourceItemIndex > targetItemIndex ? targetItemIndex + 1 : targetItemIndex);
+        UpdatePage();
+
+        ClearDragInfo();
+    }
+
+
+    private static object? GetDataFromListBox(ItemsControl source, Point point)
+    {
+        if (source.InputHitTest(point) is not UIElement hitTestElement) return null;
+        var element = hitTestElement;
+        var data = DependencyProperty.UnsetValue;
+        while (data == DependencyProperty.UnsetValue)
+        {
+            data = source.ItemContainerGenerator.ItemFromContainer(element!);
+
+            if (data == DependencyProperty.UnsetValue) element = VisualTreeHelper.GetParent(element!) as UIElement;
+
+            if (element == source) return null;
+        }
+
+        return data != DependencyProperty.UnsetValue ? data : null;
+    }
+
+    public void ClearDragInfo()
+    {
+        isDraging = false;
+        this.DragTargetItem = null;
     }
 }
--- src/Demo/WpfAppForZoomInAndZoomOut/Views/ChildView1.xaml.cs
+++ src/Demo/WpfAppForZoomInAndZoomOut/Views/ChildView1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -10,7 +11,6 @@ namespace WpfAppForZoomInAndZoomOut;
 
 public partial class ChildView1 : UserControl
 {
-    private bool isDraging;
 
     public ChildView1()
     {
@@ -64,81 +64,16 @@ public partial class ChildView1 : UserControl
 
     private void TestListBoxIns_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        var parent = (ListBox)sender;
-        ViewModel!.SourcePosition = e.GetPosition(parent);
-        var hitTestControl = VisualTreeHelper.HitTest(parent, ViewModel!.SourcePosition);
-
-        if (hitTestControl is
-            { VisualHit: Path { Name: "Path_DragPosition" } or Border { Name: "Border_DragPosition" } })
-        {
-            ViewModel!.DragSourceItem =
-                GetDataFromListBox(parent, ViewModel!.SourcePosition) as TreeItemModel;
-            if (ViewModel!.DragSourceItem != null)
-            {
-                isDraging = true;
-                DragDrop.DoDragDrop(parent, ViewModel!.DragSourceItem, DragDropEffects.Move);
-            }
-            else
-            {
-                isDraging = false;
-            }
-        }
-        else
-        {
-            isDraging = false;
-        }
+        ViewModel!.DragStart(sender,e);
     }
 
     private void TestListBoxIns_OnDragOver(object sender, DragEventArgs e)
     {
-        if (!isDraging) return;
-
-        var parent = (ListBox)sender;
-        ViewModel!.TargetPosition = e.GetPosition(parent);
-        ViewModel!.DragTargetItem = GetDataFromListBox(parent, ViewModel!.TargetPosition) as TreeItemModel;
-        ViewModel!.DragInsertPosition = (int)((ViewModel!.TargetPosition.X - ViewModel!.SourcePosition.X) / 5);
+        ViewModel!.DragOver(sender,e);
     }
 
     private void TestListBoxIns_OnDrop(object sender, DragEventArgs e)
     {
-        var parent = (ListBox)sender;
-
-        if (!isDraging || ViewModel!.DragSourceItem == null || ViewModel!.DragTargetItem == null) return;
-
-
-        var sourceItemIndex = ViewModel.ItemSource.IndexOf(ViewModel!.DragSourceItem);
-        var targetItemIndex = ViewModel.ItemSource.IndexOf(ViewModel!.DragTargetItem);
-
-        var left = ViewModel!.DragTargetItem.Margin.Left;
-        left += ChildView1ViewModel.MoveStep * ViewModel!.DragInsertPosition;
-
-        ViewModel!.DragSourceItem.Margin = new Thickness(left, 0, 0, 0);
-        ViewModel.ItemSource.Move(sourceItemIndex,
-            sourceItemIndex > targetItemIndex ? targetItemIndex + 1 : targetItemIndex);
-        ViewModel.UpdatePage();
-        ViewModel.DragTargetItem = null;
-
-        isDraging = false;
-    }
-
-    private static object GetDataFromListBox(ListBox source, Point point)
-    {
-        var element = source.InputHitTest(point) as UIElement;
-        if (element != null)
-        {
-            var data = DependencyProperty.UnsetValue;
-            while (data == DependencyProperty.UnsetValue)
-            {
-                data = source.ItemContainerGenerator.ItemFromContainer(element);
-
-                if (data == DependencyProperty.UnsetValue) element = VisualTreeHelper.GetParent(element) as UIElement;
-
-                if (element == source) return null;
-            }
-
-            if (data != DependencyProperty.UnsetValue) return data;
-        }
-
-        return null;
+        ViewModel!.Drop(sender,e);
     }
 }
