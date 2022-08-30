c15118f80bcf883b312790c89405e2aceda4c8b5
Dotnet9 <1012434131@qq.com>
2022-08-30 14:43:41 +0800

    添加数据模板右键菜单测试；

--- src/Demo/WpfAppForZoomInAndZoomOut/ViewModels/TestWindowsViewModel.cs
+++ src/Demo/WpfAppForZoomInAndZoomOut/ViewModels/TestWindowsViewModel.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace WpfAppForZoomInAndZoomOut.ViewModels;
 
 public class TestWindowsViewModel : BindableBase
 {
-    private ObservableCollection<string>? _address;
-
     private readonly List<AddressViewModel> _addressViewModels;
+    private ObservableCollection<string>? _address;
 
     private string? _selectedAddress;
 
     private AddressViewModel? _selectedAddressViewModel;
+    private ICommand? showMessageCommand;
 
     public TestWindowsViewModel()
     {
         Address = new ObservableCollection<string>(new[] { "张三", "李四", "王五" });
         _addressViewModels = new List<AddressViewModel>();
         for (var i = 0; i < 3; i++) _addressViewModels.Add(new AddressViewModel { Name = Address[i] });
+        SelectedAddress = Address[0];
     }
 
     public ObservableCollection<string>? Address
@@ -41,5 +46,13 @@ public class TestWindowsViewModel : BindableBase
     {
         get => _selectedAddressViewModel;
         set => SetProperty(ref _selectedAddressViewModel, value);
+    }
+
+    public ICommand ShowMessageCommand =>
+        showMessageCommand ??= new DelegateCommand<object>(ExecuteShowMessageCommand);
+
+    private void ExecuteShowMessageCommand(object? obj)
+    {
+        MessageBox.Show(obj == null ? DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss zzz") : obj.ToString());
     }
 }
