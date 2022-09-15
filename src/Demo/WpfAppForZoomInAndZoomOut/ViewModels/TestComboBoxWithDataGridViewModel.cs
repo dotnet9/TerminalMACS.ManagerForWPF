c50a4ecb5bfbb8a45f408b27a44cea804c371235
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-09-15 15:42:46 +0800

    添加ComboBox下拉样式测试；

---
+++ src/Demo/WpfAppForZoomInAndZoomOut/ViewModels/TestComboBoxWithDataGridViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace WpfAppForZoomInAndZoomOut.ViewModels;
+
+public class TestComboBoxWithDataGridViewModel : BindableBase
+{
+    public ObservableCollection<City> Cities { get; } = new()
+    {
+        new City() { Name = "Mumbai", State = "Maharashtra", Population = 3000000 },
+        new City() { Name = "Pune", State = "Maharashtra", Population = 7000000 },
+        new City() { Name = "Nashik", State = "Maharashtra", Population = 65000 },
+        new City() { Name = "Aurangabad", State = "Maharashtra", Population = 5000000 }
+    };
+}
+
+public class City
+{
+    public string State { get; set; }
+    public string Name { get; set; }
+    public int Population { get; set; }
+}
---
+++ src/Demo/WpfAppForZoomInAndZoomOut/Views/TestComboBoxWithDataGridView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using WpfAppForZoomInAndZoomOut.ViewModels;
+
+namespace WpfAppForZoomInAndZoomOut.Views;
+
+public partial class TestComboBoxWithDataGridView : Window
+{
+    public TestComboBoxWithDataGridViewModel? ViewModel
+    {
+        get => DataContext as TestComboBoxWithDataGridViewModel;
+        set => DataContext = value;
+    }
+
+    public TestComboBoxWithDataGridView()
+    {
+        ViewModel = new TestComboBoxWithDataGridViewModel();
+        InitializeComponent();
+    }
+}
