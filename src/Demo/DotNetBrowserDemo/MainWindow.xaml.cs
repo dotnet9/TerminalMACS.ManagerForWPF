925bbe96cba5c31baff27bcd5d91ad8763948dd6
Dotnet9 <1012434131@qq.com>
2022-09-19 11:18:49 +0800

    添加导航网址切换和语言切换测试；

--- src/Demo/DotNetBrowserDemo/MainWindow.xaml.cs
+++ src/Demo/DotNetBrowserDemo/MainWindow.xaml.cs
@@ -1,24 +1,69 @@
-﻿namespace DotNetBrowserDemo;
+﻿using System;
+
+namespace DotNetBrowserDemo;
 
 public partial class MainWindow : Window
 {
-    private readonly IEngine engine;
+    private IEngine? engine;
+    private IBrowser? browser;
+    private string? currentUrl="www.baidu.com";
 
     public MainWindow()
     {
-        engine = EngineFactory.Create();
+        CreateEngine(DotNetBrowser.Ui.Language.Chinese);
+    }
 
-        var browser = engine.CreateBrowser();
-        browser.Navigation.LoadUrl("https://dotnet9.com");
+    private void CreateEngine(DotNetBrowser.Ui.Language language)
+    {
+        DisposeEngine();
+        engine = EngineFactory.Create(new EngineOptions.Builder
+        {
+            Language = language
+        }.Build());
+
+        browser = engine.CreateBrowser();
+        browser.Navigation.LoadUrl(currentUrl);
 
         InitializeComponent();
-
+        
         this.MyBrowserView.InitializeFrom(browser);
-        Closed += MainWindow_Closed;
+    }
+
+    private void DisposeEngine()
+    {
+        if (engine == null)
+        {
+            return;
+        }
+        engine.Dispose();
+        engine = null;
     }
 
     private void MainWindow_Closed(object? sender, System.EventArgs e)
     {
-        engine.Dispose();
+        engine?.Dispose();
+    }
+
+    private void ChangeRightUrl_Click(object sender, RoutedEventArgs e)
+    {
+        string[] rightUrls = { "www.baidu.com", "www.google.com", "https://dotnet9.com" };
+        currentUrl = rightUrls[Random.Shared.Next(rightUrls.Length)];
+        browser.Navigation.LoadUrl(currentUrl);
+    }
+
+    private void ChangeFalseUrl_Click(object sender, RoutedEventArgs e)
+    {
+        currentUrl = $"www.{Random.Shared.Next(100000)}.com";
+        browser.Navigation.LoadUrl(currentUrl);
+    }
+
+    private void ChangeToChinese_Click(object sender, RoutedEventArgs e)
+    {
+        CreateEngine(DotNetBrowser.Ui.Language.Chinese);
+    }
+
+    private void ChangeToEnglish_Click(object sender, RoutedEventArgs e)
+    {
+        CreateEngine(DotNetBrowser.Ui.Language.EnglishUs);
     }
 }
