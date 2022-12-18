7cd5e9618e446f3152239e435e323adfdfdfec5d
Dotnet9(dotnet9.com) <1012434131@qq.com>
2022-12-18 21:51:41 +0800

    添加黑白主题动态切换测试；

---
+++ src/Demo/WpfThemeDemo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace WpfThemeDemo;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ChangeTheme_Click(object sender, RoutedEventArgs e)
+    {
+        ResourceDictionary resource = new();
+        if (Application.Current.Resources.MergedDictionaries[0].Source.ToString() ==
+            "pack://application:,,,/WpfThemeDemo;component/Resources/Light.xaml")
+        {
+            resource.Source = new Uri("pack://application:,,,/WpfThemeDemo;component/Resources/Dark.xaml");
+        }
+        else
+        {
+            resource.Source = new Uri("pack://application:,,,/WpfThemeDemo;component/Resources/Light.xaml");
+        }
+
+        Application.Current.Resources.MergedDictionaries[0] = resource;
+    }
+}
