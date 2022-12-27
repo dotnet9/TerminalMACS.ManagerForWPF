bb0ed60240a4da3b8e1a57b5f28ff82c114efb73
Dotnet9 <1012434131@qq.com>
2022-12-27 15:12:07 +0800

    了解rx.net;

---
+++ src/Demo/RXNetTest/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Reactive.Subjects;
+
+var inputs = new Subject<string>();
+inputs.Subscribe(Console.WriteLine);
+for (var i = 0; i < 10; i++)
+{
+    inputs.OnNext(i.ToString());
+}
+
+Console.ReadLine();
