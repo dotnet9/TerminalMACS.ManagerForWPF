f2610675ec581594024720f0b6cfd49fea2992d8
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-02-08 11:29:57 +0800

    添加ribbon和dock测试案例；
    代码直接拷贝自这个仓库：https://gitee.com/rpa_ai/rpastudiobook/blob/master/books/zh/preface.md；

---
+++ src/Demo/WorkflowTest/RibbonDockSample/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using RibbonDockSample.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RibbonDockSample
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FluentRibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new FluentRibbonWindow();
+            window.Show();
+        }
+
+        private void AvalonDockButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new AvalonDockWindow();
+            window.Show();
+        }
+
+        private void FluentRibbonAndAvalonDockButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new FluentRibbonAndAvalonDockWindow();
+            window.Show();
+        }
+
+        private void ActiproRibbonDockButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ActiproRibbonDockWindow();
+            window.Show();
+        }
+    }
+}
---
+++ src/Demo/WorkflowTest/RibbonDockSample/Windows/ActiproRibbonDockWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ActiproSoftware.Windows.Controls.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RibbonDockSample.Windows
+{
+    /// <summary>
+    /// ActiproRibbonDockWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ActiproRibbonDockWindow : RibbonWindow
+    {
+        public ActiproRibbonDockWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
