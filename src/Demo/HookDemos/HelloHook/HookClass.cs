6d8d6036ccb929de7dbca7ff0e8f0b118a77b5c1
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-02-20 22:44:40 +0800

    降低hook demo的.net 版本为.net 6，.net 7问题；
    https://github.com/pardeike/Harmony/issues/504

--- src/Demo/HookDemos/HelloHook/HookClass.cs
+++ src/Demo/HookDemos/HelloHook/HookClass.cs
@@ -7,7 +7,7 @@ namespace HelloHook;
 [HarmonyPatch(nameof(Student.GetDetails))] // 定义拦截的方法：GetDetail
 public class HookClass
 {
-    public static bool Prefix(ref string name)
+    public static bool Prefix(ref string name, ref string __result)
     {
         Console.WriteLine($"Prefix");
         if (!"沙漠尽头的狼".Equals(name))
@@ -15,11 +15,13 @@ public class HookClass
             name = "假的，千万别信";
         }
 
-        return true;
+        __result = $"不确定'{name}'是哪个网站的站长";
+        return false;
     }
 
-    public static void Postfix(string name, ref string __result)
+    public static void Postfix(ref string name, ref string __result)
     {
+        name = "3";
         __result = $"不确定'{name}'是哪个网站的站长";
         Console.WriteLine($"Postfix");
     }
---
+++ src/Demo/HookDemos/HookDotnetAPI/Program.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+
+var dotnet9Domain = "https://dotnet9.com";
+Console.WriteLine($"9的位置：{dotnet9Domain.IndexOf('9',0)}");
+
+var harmony = new Harmony("com.dotnet9");
+harmony.PatchAll();
+
+Console.WriteLine($"9的位置：{dotnet9Domain.IndexOf('9', 0)}");
+
+[HarmonyPatch(typeof(String))]
+[HarmonyPatch(nameof(string.IndexOf))]
+[HarmonyPatch(new Type[] { typeof(char), typeof(int) })]
+public static class HookClass
+{
+    public static bool Prefix(ref int __result)
+    {
+        __result = 100;
+        return false;
+    }
+}
