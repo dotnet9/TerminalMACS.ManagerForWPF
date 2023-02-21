2c925584ad406497c5ccbb0b48067789661e7afd
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-02-21 20:10:34 +0800

    添加返回结构体测试；

--- src/Demo/HookDemos/HookDotnetAPI/Program.cs
+++ src/Demo/HookDemos/HookDotnetAPI/Program.cs
@@ -1,12 +1,14 @@
 ﻿using HarmonyLib;
+using System.Xml.Linq;
 
 var dotnet9Domain = "https://dotnet9.com";
-Console.WriteLine($"9的位置：{dotnet9Domain.IndexOf('9',0)}");
+Console.WriteLine($"9的位置：{dotnet9Domain.IndexOf('9', 0)}");
 
 var harmony = new Harmony("com.dotnet9");
 harmony.PatchAll();
 
 Console.WriteLine($"9的位置：{dotnet9Domain.IndexOf('9', 0)}");
+Console.WriteLine(new PointHelper().GetPoint("名称"));
 
 [HarmonyPatch(typeof(String))]
 [HarmonyPatch(nameof(string.IndexOf))]
@@ -17,5 +19,43 @@ public static class HookClass
     {
         __result = 100;
         return false;
+    }
+}
+
+[HarmonyPatch(typeof(PointHelper))]
+[HarmonyPatch(nameof(PointHelper.GetPoint))]
+public static class HookPointHelper
+{
+    public static bool Prefix(ref string name, ref Point __result)
+    {
+        name = "test";
+        __result = new Point() { X = 2, Y = 3 };
+        return false;
+    }
+}
+
+public class PointHelper
+{
+    public Point GetPoint(string name)
+    {
+        Console.WriteLine($"收到参数：{name}");
+        return new Point { X = 100, Y = 100 };
+    }
+
+    public string SetPoint(Point point)
+    {
+        Console.WriteLine($"收到参数,x={point.X},y={point.Y}");
+        return "test";
+    }
+}
+
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString()
+    {
+        return $"x={X},y={Y}";
     }
 }
