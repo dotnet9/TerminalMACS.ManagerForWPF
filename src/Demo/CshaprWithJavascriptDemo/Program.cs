3c97a4a3ac2779cf1faa8905bfa43385a91962fd
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-03-03 17:38:33 +0800

    添加JS引擎执行测试代码；

--- src/Demo/CshaprWithJavascriptDemo/Program.cs
+++ src/Demo/CshaprWithJavascriptDemo/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace CshaprWithJavascriptDemo
-{
+﻿using Microsoft.ClearScript.V8;
+using System;
+
+namespace CshaprWithJavascriptDemo {
     internal class Program
     {
         static void Main(string[] args)
         {
+            using var engine = new V8ScriptEngine();
+            //engine.DocumentSettings.AccessFlags = Microsoft.ClearScript.DocumentAccessFlags.EnableFileLoading;
+            //engine.DefaultAccess = Microsoft.ClearScript.ScriptAccess.Full; // 这两行是为了允许加载js文件
+            // do something
+            var jsFunction = @"function say(name){
+                            return 'Hello '+name;    
+                        }";
+            engine.Execute(jsFunction);
+            var result = engine.Script.say("沙漠尽头的狼");
+            Console.WriteLine(result);
         }
     }
 }
