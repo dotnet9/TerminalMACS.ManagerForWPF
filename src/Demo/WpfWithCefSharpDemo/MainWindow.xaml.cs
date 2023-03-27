ff791705547d9e6f0f6b0fbf5e4259dd228ea834
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-03-27 23:26:14 +0800

    添加完整的示例；

--- src/Demo/WpfWithCefSharpDemo/MainWindow.xaml.cs
+++ src/Demo/WpfWithCefSharpDemo/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CefSharp;
+using Newtonsoft.Json;
+using System;
 using System.IO;
-using CefSharp;
+using System.Text;
 using System.Windows;
 
 namespace WpfWithCefSharpDemo
@@ -10,35 +12,71 @@ namespace WpfWithCefSharpDemo
         public MainWindow()
         {
             InitializeComponent();
+
+            // 允许以同步的方式注册C#的对象到JS中
+            Browser.JavascriptObjectRepository.Settings.LegacyBindingEnabled = true;
+            CefSharpSettings.WcfEnabled = true;
+
+            // 注册C#的对象到JS中的代码必须在Cef的Browser加载之前调用
+            Browser.JavascriptObjectRepository.Register("cefSharpExample", new CefSharpExample(), false,
+                options: BindingOptions.DefaultBinder);
         }
 
-        private void Browser_Loaded(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Cef浏览器控件加载完成后，加载网页内容，可以加载网页的Url，也可以加载网页内容
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Browser_OnLoaded(object sender, RoutedEventArgs e)
         {
-            Browser.RegisterName("cefSharpExample", new CefSharpExample(this));
             var htmlFile = $"{AppDomain.CurrentDomain.BaseDirectory}test.html";
-            if (File.Exists(htmlFile))
+            if (!File.Exists(htmlFile))
             {
-                Browser.LoadHtml(htmlFile);
+                return;
             }
+
+            var htmlContent = File.ReadAllText(htmlFile, Encoding.UTF8);
+            Browser.LoadHtml(htmlContent);
+        }
+
+        /// <summary>
+        /// C#里调用JS的一般方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CallJSFunc_Click(object sender, RoutedEventArgs e)
+        {
+            var jsCode = $"displayMessage('C#里的调用')";
+            Browser.ExecuteScriptAsync(jsCode);
+        }
+
+        /// <summary>
+        /// C#调用一个JS的方法，并传递一个JSON对象
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SendJsonToWeb_Click(object sender, RoutedEventArgs e)
+        {
+            var jsonContent = new
+            {
+                Id = 1,
+                Name = "沙漠尽头的狼",
+                Age = 25,
+                WebSite="https://dotnet9.com"
+            };
+            var jsonStr = JsonConvert.SerializeObject(jsonContent);
+
+            // 传递Json对象，即传递一个JSON字符串，和前面的一个示例一样
+            var jsCode = $"displayJson('{jsonStr}')";
+            Browser.ExecuteScriptAsync(jsCode);
         }
     }
 
     public class CefSharpExample
     {
-        private MainWindow mainWindow;
-
-        public CefSharpExample(MainWindow mainWindow)
-        {
-            this.mainWindow = mainWindow;
-        }
-
         public void TestMethod(string message)
         {
-            mainWindow.Dispatcher.Invoke(() =>
-            {
-                var jsCode = $"displayMessage('{message}')";
-                mainWindow.Browser.ExecuteScriptAsync(jsCode);
-            });
+            Application.Current.Dispatcher.Invoke(() => { MessageBox.Show("JS里的调用"); });
         }
     }
 }
