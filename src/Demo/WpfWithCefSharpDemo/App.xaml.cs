dc9b4185566be589a973c7e3d43115ded0973dd5
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-03-27 17:52:32 +0800

    添加Cef测试；

--- src/Demo/WpfWithCefSharpDemo/App.xaml.cs
+++ src/Demo/WpfWithCefSharpDemo/App.xaml.cs
@@ -6,12 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfWithWebView2
+namespace WpfWithCefSharpDemo
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
 }
---
+++ src/Demo/WpfWithCefSharpDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using CefSharp;
+using System.Windows;
+
+namespace WpfWithCefSharpDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Browser_Loaded(object sender, RoutedEventArgs e)
+        {
+            Browser.RegisterName("cefSharpExample", new CefSharpExample(this));
+            var htmlFile = $"{AppDomain.CurrentDomain.BaseDirectory}test.html";
+            if (File.Exists(htmlFile))
+            {
+                Browser.LoadHtml(htmlFile);
+            }
+        }
+    }
+
+    public class CefSharpExample
+    {
+        private MainWindow mainWindow;
+
+        public CefSharpExample(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+        public void TestMethod(string message)
+        {
+            mainWindow.Dispatcher.Invoke(() =>
+            {
+                var jsCode = $"displayMessage('{message}')";
+                mainWindow.Browser.ExecuteScriptAsync(jsCode);
+            });
+        }
+    }
+}
