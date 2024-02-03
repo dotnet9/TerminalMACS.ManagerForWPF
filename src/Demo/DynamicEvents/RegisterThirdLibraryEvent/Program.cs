d0badac017dd517b0e14d3c885b805328a403793
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-02-03 22:57:25 +0800

    完成动态注册事件备注；

--- src/Demo/DynamicEvents/RegisterThirdLibraryEvent/Program.cs
+++ src/Demo/DynamicEvents/RegisterThirdLibraryEvent/Program.cs
@@ -1,69 +1,58 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 
 // 加载第三方库  
 var assembly = Assembly.LoadFrom("ThirdLibrary.dll");
 
 // 创建TestClass的实例  
 var testClassType = assembly.GetType("ThirdLibrary.TestClass");
-var testClassInstance = Activator.CreateInstance(testClassType);
-var fields = testClassType.GetFields();
+var testClassInstance = Activator.CreateInstance(testClassType!);
+var fields = testClassType!.GetFields();
 
-// 1|获取NoParamEvent委托  
+// 1、获取NoParamEvent委托  
 var noParamEventField = fields.First(field => "NoParamEvent" == field.Name);
-var noParamEventActionType = typeof(Action);
-var noParamEventHandler = new Action(EventHandlerMethod);
-noParamEventField.SetValue(testClassInstance, noParamEventHandler);
+noParamEventField.SetValue(testClassInstance, EventHandlerMethod);
 
-// 2|获取OneParamEvent委托，并设置事件参数处理程序  
+// 2、获取OneParamEvent委托，并设置事件参数处理程序  
 var oneParamEventField = fields.First(field => "OneParamEvent" == field.Name);
-var oneParamEventActionType = typeof(Action<>).MakeGenericType(typeof(string));
-var oneParamEventHandler = new Action<string>(OneParamEventHandler);
-oneParamEventField.SetValue(testClassInstance, oneParamEventHandler);
+oneParamEventField.SetValue(testClassInstance, OneParamEventHandler);
 
-// 3|获取TwoParamEvent委托，并设置事件参数处理程序  
+// 3、获取TwoParamEvent委托，并设置事件参数处理程序  
 var twoParamEventField = fields.First(field => "TwoParamEvent" == field.Name);
-var twoParamEventHandler = new Action<string, dynamic>(TwoParamEventHandler); // 使用dynamic作为第二个参数的类型  
-twoParamEventField.SetValue(testClassInstance, twoParamEventHandler);
-
-// 4.1|EventHandler
-//var eventHandlerEventField = fields.First(field => "EventHandlerEvent" == field.Name);
-//var eventHandlerMethod = new Action<object, dynamic>(EventHandlerEventHandler);  
-//twoParamEventField.SetValue(testClassInstance, twoParamEventHandler);
+twoParamEventField.SetValue(testClassInstance, TwoParamEventHandler);
 
 var events = testClassType.GetEvents();
 
-// 4.2|获取EventHandler事件
+// 4、获取EventHandler事件
 var eventHandlerEventField = events.First(item => "EventHandlerEvent" == item.Name);
 var eventHandlerType = eventHandlerEventField.EventHandlerType;
 var eventHandlerMethod = new EventHandler<dynamic>(EventHandlerEventHandler);
 var handle = Delegate.CreateDelegate(eventHandlerType, eventHandlerMethod.Method);
 eventHandlerEventField.AddEventHandler(null, handle);
 
-// 模拟触发事件通知
-var callEventMethod = testClassType.GetMethods().First(method => "CallTwoParamEvent" == method.Name);
+// 5、模拟触发事件通知，测试事件是否注册成功
+var callEventMethod = testClassType.GetMethods().First(method => "CallEvent" == method.Name);
 callEventMethod.Invoke(testClassInstance, null);
 
 // NoParamEvent事件处理程序方法  
 void EventHandlerMethod()
 {
-    Console.WriteLine("NoParamEvent event raised.");
+    Console.WriteLine("NoParamEvent： event raised.");
 }
 
 // OneParamEvent事件处理程序方法，需要一个字符串参数  
 void OneParamEventHandler(string param)
 {
-    Console.WriteLine($"OneParamEvent event raised with parameter: {param}");
+    Console.WriteLine($"OneParamEvent： event raised with parameter: {param}");
 }
 
 // TwoParamEvent事件处理程序方法，需要两个参数：string和EventParam类型（通过反射传递，EventParam类型使用动态类型dynamic替换）  
 void TwoParamEventHandler(string param1, dynamic param2) // 使用dynamic作为第二个参数的类型，并通过反射传递实际参数值  
 {
-    Console.WriteLine($"param1={param1}, param2.Param1={param2.Message}");
+    Console.WriteLine($"TwoParamEvent： event raised, param1={param1}, param2.Param1={param2.Message}");
 }
 
+// EventHandler事件处理方法
 void EventHandlerEventHandler(object sender, dynamic param)
 {
-   Console.WriteLine($"param.Param1={param.Message}");
+    Console.WriteLine($"EventHandler: param.Param1={param.Message}");
 }
--- src/Demo/DynamicEvents/ThirdLibrary/TestClass.cs
+++ src/Demo/DynamicEvents/ThirdLibrary/TestClass.cs
@@ -2,20 +2,42 @@
 
 public class TestClass
 {
+    /// <summary>
+    /// 无参委托
+    /// </summary>
     public Action? NoParamEvent;
 
+    /// <summary>
+    /// 带1个string参数
+    /// </summary>
     public Action<string>? OneParamEvent;
-    public Action<string, EventParam>? TwoParamEvent;
-    public static event EventHandler<EventParam> EventHandlerEvent;
 
-    public void CallTwoParamEvent()
+    /// <summary>
+    /// 带1个基本类型和自定义类型的委托
+    /// </summary>
+    public Action<string, EventParam>? TwoParamEvent;
+
+    /// <summary>
+    /// EventHandler事件
+    /// </summary>
+    public static event EventHandler<EventParam>? EventHandlerEvent;
+
+    /// <summary>
+    /// 该方法用于触发事件，方便测试
+    /// </summary>
+    public void CallEvent()
     {
-        TwoParamEvent?.Invoke("恭喜", new EventParam() { Message = "帅哥，你成功调用啦！" });
-        EventHandlerEvent?.Invoke(this, new EventParam { Message = "EventHandler类型调用成功"});
+        NoParamEvent?.Invoke();
+        OneParamEvent?.Invoke("单参数委托");
+        TwoParamEvent?.Invoke("2个参数委托调用成功", new EventParam() { Message = "帅哥，你成功调用啦！" });
+        EventHandlerEvent?.Invoke(this, new EventParam { Message = "EventHandler事件调用成功" });
     }
 }
 
+/// <summary>
+/// 自定义类型，注册时需要使用dynamic接收
+/// </summary>
 public class EventParam
 {
-    public string Message { get; set; }
+    public string? Message { get; set; }
 }
